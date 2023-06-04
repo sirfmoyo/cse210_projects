633738f5b138d567f4571d2fc2b4e342e24b7a44
Freeman Moyo <sirfmoyo@gmail.com>
2023-06-04 14:26:08 +0200

    done week2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int score = int.Parse(userGrade);
+
+        string symbol = "";
+
+        if (score >= 90)
+        {
+            symbol = "A";
+        }
+        else if (percent >= 80)
+        {
+            symbol = "B";
+        }
+        else if (score >= 70)
+        {
+            symbol = "C";
+        }
+        else if (score >= 60)
+        {
+            symbol = "D";
+        }
+        else
+        {
+            symbol = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {symbol}");
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Wrong guess, your value is higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Wrong guess, your value is less");
+            }
+            else
+            {
+                Console.WriteLine("Correct guess!");
+            }
+
+        }                    
     }
 }
