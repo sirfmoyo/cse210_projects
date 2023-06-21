30a4f0bef7dbc6760622c515e3b37de51f658bb9
Freeman Moyo <sirfmoyo@gmail.com>
2023-06-21 05:05:34 +0200

    done week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+class Job
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Teacher";
+        job1._company = "Delta";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Teacher";
+        job2._company = "Mpopoma";
+        job2._startYear = 2009;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Freeman Moyo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
